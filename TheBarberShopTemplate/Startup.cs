ff0a3e5c17c6680d8ab505a3892517ea336c1716
joderrick1022 <joderrick1022@gmail.com>
2018-05-22 23:23:51 -0400

    Add project files.

---
+++ TheBarberShopTemplate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheBarberShopTemplate.Startup))]
+namespace TheBarberShopTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
